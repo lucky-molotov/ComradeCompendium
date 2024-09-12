2a531ba1cfc234f2a724ba6230e68289d8d0fb29
Lucky Molotov <166308508+lucky-molotov@users.noreply.github.com>
2024-09-12 22:57:03 +0200

    Updated Function and Grouped Them
    - Grouped the search functions under a search class and changed their names
    - Grouped the creation function plus the view all functions under one class and changed their names
    - Updated the menu to suit new grouping
    - Added output print to create random function

--- main.py
+++ main.py
@@ -25,264 +25,267 @@ if cursor.fetchone() is None:
 conn.commit()
 conn.close()
 
-def create_new():
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
+class comrade():
+    def create_new():
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
 
-    name = input("Input the Name: ").capitalize()
-    surname = input("Input the Surname: ").capitalize()
-    contact_number = input("Input the contact number with international code: ")
-    email = input("Input the e-mail: ")
-    date_of_birth = input("Input date of birth (YYYY-MM-DD): ")
-    relation = input("What is the relation: ")
-    location = input("Where do they live: ")
+        name = input("Input the Name: ").capitalize()
+        surname = input("Input the Surname: ").capitalize()
+        contact_number = input("Input the contact number with international code: ")
+        email = input("Input the e-mail: ").lower()
+        date_of_birth = input("Input date of birth (YYYY-MM-DD): ")
+        relation = input("What is the relation: ")
+        location = input("Where do they live: ")
 
-    # Insert data into database
-    query = """
-    INSERT INTO comrade (name, surname, contact_number, email, date_of_birth, relation, location)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
-    """
-    cursor.execute(query, (name, surname, contact_number, email, date_of_birth, relation, location))
-    conn.commit()
-    conn.close()
+        # Insert data into database
+        query = """
+        INSERT INTO comrade (name, surname, contact_number, email, date_of_birth, relation, location)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """
+        cursor.execute(query, (name, surname, contact_number, email, date_of_birth, relation, location))
+        conn.commit()
+        conn.close()
 
-    print("New comrade added successfully!")
+        print("New comrade added successfully!")
 
-def create_random_comrade():
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
+    def create_new_random():
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
 
-    names = [
-        "Liam", "Emma", "Lucas", "Sophie", "Nicolas",
-        "Vladimir", "Elena", "Mikhail", "Anastasia", "Mateusz",
-        "Isabella", "Javier", "Francesca", "Dimitri", "Zara",
-        "Petr", "Katarina", "Igor", "Svetlana", "Jarek",
-        "Kwame", "Amina", "Ngozi", "Kofi", "Fatima",
-        "Omar", "Layla", "Khalid", "Amira", "Yusuf",
-        "Hiroshi", "Sakura", "Chen", "Ravi", "Ayesha",
-        "Zainab", "Mohammed", "Chinedu", "Sunil", "Mei"
-    ]
+        names = [
+            "Liam", "Emma", "Lucas", "Sophie", "Nicolas",
+            "Vladimir", "Elena", "Mikhail", "Anastasia", "Mateusz",
+            "Isabella", "Javier", "Francesca", "Dimitri", "Zara",
+            "Petr", "Katarina", "Igor", "Svetlana", "Jarek",
+            "Kwame", "Amina", "Ngozi", "Kofi", "Fatima",
+            "Omar", "Layla", "Khalid", "Amira", "Yusuf",
+            "Hiroshi", "Sakura", "Chen", "Ravi", "Ayesha",
+            "Zainab", "Mohammed", "Chinedu", "Sunil", "Mei"
+        ]
 
-    surnames = [
-        "Ivanov", "Nowak", "Kovacs", "Petrov", "Stoica",
-        "Müller", "Dupont", "Martínez", "Rossi", "Smith",
-        "Okafor", "Diop", "Banda", "Abebe", "Nguyen",
-        "Al-Masri", "Hassan", "Rahman", "Farouk", "Nasser",
-        "Tanaka", "Wang", "Kim", "Singh", "Li",
-        "Mbatha", "Osei", "Khan", "Jiang", "Patel"
-    ]
+        surnames = [
+            "Ivanov", "Nowak", "Kovacs", "Petrov", "Stoica",
+            "Müller", "Dupont", "Martínez", "Rossi", "Smith",
+            "Okafor", "Diop", "Banda", "Abebe", "Nguyen",
+            "Al-Masri", "Hassan", "Rahman", "Farouk", "Nasser",
+            "Tanaka", "Wang", "Kim", "Singh", "Li",
+            "Mbatha", "Osei", "Khan", "Jiang", "Patel"
+        ]
 
-    locations = [
-        "London, United Kingdom", "Paris, France", "Berlin, Germany", "Rome, Italy", "Madrid, Spain",
-        "Moscow, Russia", "Warsaw, Poland", "Prague, Czech Republic", "Budapest, Hungary", "Bucharest, Romania",
-        "Lagos, Nigeria", "Cairo, Egypt", "Johannesburg, South Africa", "Nairobi, Kenya", "Accra, Ghana",
-        "Riyadh, Saudi Arabia", "Dubai, UAE", "Amman, Jordan", "Beirut, Lebanon", "Algiers, Algeria",
-        "Tokyo, Japan", "Beijing, China", "Mumbai, India", "Seoul, South Korea", "Bangkok, Thailand",
-        "Casablanca, Morocco", "Dakar, Senegal", "Tehran, Iran", "Hanoi, Vietnam", "Manila, Philippines"
-    ]
+        locations = [
+            "London, United Kingdom", "Paris, France", "Berlin, Germany", "Rome, Italy", "Madrid, Spain",
+            "Moscow, Russia", "Warsaw, Poland", "Prague, Czech Republic", "Budapest, Hungary", "Bucharest, Romania",
+            "Lagos, Nigeria", "Cairo, Egypt", "Johannesburg, South Africa", "Nairobi, Kenya", "Accra, Ghana",
+            "Riyadh, Saudi Arabia", "Dubai, UAE", "Amman, Jordan", "Beirut, Lebanon", "Algiers, Algeria",
+            "Tokyo, Japan", "Beijing, China", "Mumbai, India", "Seoul, South Korea", "Bangkok, Thailand",
+            "Casablanca, Morocco", "Dakar, Senegal", "Tehran, Iran", "Hanoi, Vietnam", "Manila, Philippines"
+        ]
 
-    name = random.choice(names)
-    surname = random.choice(surnames)
-    contact_number = f"+{random.randint(1000000000, 9999999999)}"
-    email = f"{name}{surname}{random.randint(10,1000)}@example.com"
-    date_of_birth = f"{random.randint(1900, 2000)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}"
-    relation = random.choice(["Friend", "Family Member", "Colleague"])
-    location = random.choice(locations)
+        name = random.choice(names)
+        surname = random.choice(surnames)
+        contact_number = f"+{random.randint(1000000000, 9999999999)}"
+        email = f"{name}{surname}{random.randint(10,1000)}@example.com".lower()
+        date_of_birth = f"{random.randint(1950, 2006)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}"
+        relation = random.choice(["Friend", "Family Member", "Colleague"])
+        location = random.choice(locations)
 
-    query = """
-    INSERT INTO comrade (name, surname, contact_number, email, date_of_birth, relation, location)
-    VALUES (?, ?, ?, ?, ?, ?, ?)
-    """
-    cursor.execute(query, (name, surname, contact_number, email, date_of_birth, relation, location))
-    conn.commit()
-    conn.close()
+        query = """
+        INSERT INTO comrade (name, surname, contact_number, email, date_of_birth, relation, location)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+        """
+        cursor.execute(query, (name, surname, contact_number, email, date_of_birth, relation, location))
+        conn.commit()
+        conn.close()
+        print("New random comrade added successfully!")
 
-def view_all():
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade"
-    cursor.execute(query)
-    rows = cursor.fetchall()
+    def delete():
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+        comrade_id = input("Enter the ID of the comrade to delete: ")
+        query = "DELETE FROM comrade WHERE id = ?"
+        cursor.execute(query, (comrade_id,))
+        conn.commit()
+        conn.close()
 
-    for row in rows:
-        pt.add_row(row)
+        print("Comrade deleted successfully!")
+            
+    def view_all():
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade"
+        cursor.execute(query)
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def search_by_name():
-    name = input("Search By Name: ")
+        for row in rows:
+            pt.add_row(row)
 
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade WHERE LOWER(name) LIKE LOWER(?)"
-    cursor.execute(query, ('%' + name.lower() + '%',))
-    rows = cursor.fetchall()
+        print(pt)
+        conn.close()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+class search():
+    
+    def by_name():
+        name = input("Search By Name: ")
 
-    for row in rows:
-        pt.add_row(row)
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade WHERE LOWER(name) LIKE LOWER(?)"
+        cursor.execute(query, ('%' + name.lower() + '%',))
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def search_by_surname():
-    surname = input("Search By Surname: ")
+        for row in rows:
+            pt.add_row(row)
 
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade WHERE LOWER(surname) LIKE LOWER(?)"
-    cursor.execute(query, ('%' + surname.lower() + '%',))
-    rows = cursor.fetchall()
+        print(pt)
+        conn.close()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+    def by_surname():
+        surname = input("Search By Surname: ")
 
-    for row in rows:
-        pt.add_row(row)
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade WHERE LOWER(surname) LIKE LOWER(?)"
+        cursor.execute(query, ('%' + surname.lower() + '%',))
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def search_by_full_name():
-    full_name = input("Search By Full Name: ").lower()
-    name, surname = full_name.split()
+        for row in rows:
+            pt.add_row(row)
 
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade WHERE LOWER(name) = ? AND LOWER(surname) = ?"
-    cursor.execute(query, (name, surname))
-    rows = cursor.fetchall()
+        print(pt)
+        conn.close()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+    def by_full_name():
+        full_name = input("Search By Full Name: ").lower()
+        name, surname = full_name.split()
 
-    for row in rows:
-        pt.add_row(row)
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade WHERE LOWER(name) = ? AND LOWER(surname) = ?"
+        cursor.execute(query, (name, surname))
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def search_by_email():
-    email = input("Search By Email: ")
+        for row in rows:
+            pt.add_row(row)
 
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade WHERE LOWER(email) LIKE LOWER(?)"
-    cursor.execute(query, ('%' + email.lower() + '%',))
-    rows = cursor.fetchall()
+        print(pt)
+        conn.close()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+    def by_email():
+        email = input("Search By Email: ")
 
-    for row in rows:
-        pt.add_row(row)
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade WHERE LOWER(email) LIKE LOWER(?)"
+        cursor.execute(query, ('%' + email.lower() + '%',))
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def search_by_location():
-    location = input("Search By Location: ")
+        for row in rows:
+            pt.add_row(row)
 
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade WHERE LOWER(location) LIKE LOWER(?)"
-    cursor.execute(query, ('%' + location.lower() + '%',))
-    rows = cursor.fetchall()
+        print(pt)
+        conn.close()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+    def by_location():
+        location = input("Search By Location: ")
 
-    for row in rows:
-        pt.add_row(row)
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade WHERE LOWER(location) LIKE LOWER(?)"
+        cursor.execute(query, ('%' + location.lower() + '%',))
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def search_by_relation():
-    relation = input("Search By Relation: ")
+        for row in rows:
+            pt.add_row(row)
 
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
-    query = "SELECT * FROM comrade WHERE LOWER(relation) LIKE LOWER(?)"
-    cursor.execute(query, ('%' + relation.lower() + '%',))
-    rows = cursor.fetchall()
+        print(pt)
+        conn.close()
 
-    pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
-    pt.align["ID"] = "l"
-    pt.align["Name"] = "l"
-    pt.align["Surname"] = "l"
-    pt.align["Contact Number"] = "l"
-    pt.align["Email"] = "l"
-    pt.align["Date of Birth"] = "l"
-    pt.align["Relation"] = "l"
-    pt.align["Location"] = "l"
+    def by_relation():
+        relation = input("Search By Relation: ")
 
-    for row in rows:
-        pt.add_row(row)
+        conn = sqlite3.connect('comrade.db')
+        cursor = conn.cursor()
+        query = "SELECT * FROM comrade WHERE LOWER(relation) LIKE LOWER(?)"
+        cursor.execute(query, ('%' + relation.lower() + '%',))
+        rows = cursor.fetchall()
 
-    print(pt)
-    conn.close()
+        pt = PrettyTable(['ID', 'Name', 'Surname', 'Contact Number', 'Email', 'Date of Birth', 'Relation', 'Location'])
+        pt.align["ID"] = "l"
+        pt.align["Name"] = "l"
+        pt.align["Surname"] = "l"
+        pt.align["Contact Number"] = "l"
+        pt.align["Email"] = "l"
+        pt.align["Date of Birth"] = "l"
+        pt.align["Relation"] = "l"
+        pt.align["Location"] = "l"
 
-def delete_comrade():
-    conn = sqlite3.connect('comrade.db')
-    cursor = conn.cursor()
+        for row in rows:
+            pt.add_row(row)
 
-    comrade_id = input("Enter the ID of the comrade to delete: ")
-    query = "DELETE FROM comrade WHERE id = ?"
-    cursor.execute(query, (comrade_id,))
-    conn.commit()
-    conn.close()
-
-    print("Comrade deleted successfully!")
-
+        print(pt)
+        conn.close()
 def menu():
     while True:
         print("\nComrade Compendium")
@@ -301,25 +304,27 @@ def menu():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            create_new()
+            comrade.create_new()
         elif choice == '2':
-            create_random_comrade()
+            comrade.create_new_random()
         elif choice == '3':
-            view_all()
+            comrade.view_all()
         elif choice == '4':
-            search_by_name()
+            search.by_name()
         elif choice == '5':
-            search_by_surname()
+            search.by_surname()
         elif choice == '6':
-            search_by_full_name()
+            search.by_full_name()
         elif choice == '7':
-            search_by_email()
+            search.by_email()
         elif choice == '8':
-            search_by_relation()
+            search.by_relation()
         elif choice == '9':
-            search_by_location()
+            search.by_location()
         elif choice == '10':
-            break
+            comrade.delete()
+        elif choice == 'q':
+            quit()
         else:
             print("Invalid choice. Please try again.")
 
